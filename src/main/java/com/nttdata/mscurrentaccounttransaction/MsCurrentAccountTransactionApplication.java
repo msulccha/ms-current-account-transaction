78021686d482340053162c714c0c9a45db3f8ea2
Mauricio Kevin Sulca Chavez <msulccha@emeal.nttdata.com>
2022-08-18 19:00:12 -0500

    primer

---
+++ src/main/java/com/nttdata/mscurrentaccounttransaction/MsCurrentAccountTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.mscurrentaccounttransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCurrentAccountTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCurrentAccountTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/mscurrentaccounttransaction/MsCurrentAccountTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.mscurrentaccounttransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCurrentAccountTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
